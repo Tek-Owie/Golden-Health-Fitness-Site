d5d798899d97816e1f5fac4bf89ccfc4aa04a2e3
Tek-Owie <74820829+Tek-Owie@users.noreply.github.com>
2021-10-12 17:02:33 +0100

    Updated IG and WhatsApp links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,10 +20,10 @@ query {
         <FooterSocialWrapper>
             <P font-size="xSmall" color="dark2" textAlign="center" style={{fontStyle: 'italic'}}>Connect with us</P>
             <FooterSocialIcons>
-                <a href="https:instagram.com/li_goldiee" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.instagram.com/golden_lilian_okeze/" target="_blank" rel="noopener noreferrer">
                     <img src={data.instagram.publicURL} alt="instagram logo"></img>
                 </a>
-                <a href="https:whatsapp.com" target="_blank" rel="noopener noreferrer">
+                <a href="https://api.whatsapp.com/send?phone=2347063319828&text=Hi,%20I'm%20interested%20in%20learning%20more%20about%20Golden%20Health%20&%20Fitness" target="_blank" rel="noopener noreferrer">
                     <img src={data.whatsapp.publicURL} alt="whatsapp logo"></img>
                 </a>
             </FooterSocialIcons>
