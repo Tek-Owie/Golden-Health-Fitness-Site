dc845f627f17df0092772aa220b8b852138cda98
Tek-Owie <74820829+Tek-Owie@users.noreply.github.com>
2021-10-12 17:29:10 +0100

    Added social media handles

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -15,7 +15,14 @@ const contactUs = ({data}) => {
             <Post>
                 <H1>GET IN TOUCH</H1>
                 <Img fluid={featureImage}/>
-                <P>Do you have questions, comment and contributions?<br/>Feel free to reach us on our social media handles: IG; FB; TW; WH<br/>Give us a shout-out and we'll be glad to respond to you ASAP!!</P>
+                <P>Do you have questions, comments and contributions?
+                    <br/>
+                    Feel free to reach us on our social media handles 
+                    <li><a href="https://www.instagram.com/golden_lilian_okeze/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+                    <li><a href="https://api.whatsapp.com/send?phone=2347063319828&text=Hi,%20I'm%20interested%20in%20learning%20more%20about%20Golden%20Health%20&%20Fitness" target="_blank" rel="noopener noreferrer">WhatsApp</a></li>
+                    <br/>
+                    Give us a shout-out and we will respond to you ASAP!
+                </P>
             </Post>
             <Footer/>
         </ContainerWrapper>
